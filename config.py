fa04f4988ced63b4bde38f3530d49b64d572fb54
Lipairui <953383269@qq.com>
2018-12-05 12:03:28 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+config = {
+    # English word2vec model path
+    'en_model_path':'../model/GoogleNews-vectors-negative300.bin', 
+    # Chinese word2vec model path
+    'cn_model_path':'../model/cn.cbow.bin',
+    # English stopwords path
+    'en_stopwords_path': '../data/english_stopwords.txt',
+    # Chinese stopwords path
+    'cn_stopwords_path': '../data/chinese_stopwords2.txt'
+}
